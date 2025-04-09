2a32ebe9e9ca75bd8ed4c35122647c8671965718
Tyler Evans <evstye@gmail.com>
2025-04-09 09:37:54 -0600

    added python i2c and GPIO outputs for intended pins

--- c_python.py
+++ c_python.py
@@ -19,9 +19,19 @@ step_size_turn = 10
 
 #gpio for I2C
 """TODO I2C is needed here!!!"""
+SDA = 2
+SCL = 3
+BUS = 1
+#this is the regs to read.
+REG = 0x00
+#this is the time of flight address found in the datasheet
+#https://www.st.com/resource/en/datasheet/vl53l0x.pdf
+DEV_ADDRESS_TOF = 0x52
+
 g1 = 16
 g2 = 20
 g3 = 21
+
 #these are the gpio pins for motor controler
 m1 = 5
 m2 = 6
@@ -35,7 +45,55 @@ re2 = 22
 le1 = 23
 le2 = 24
 
-def option(v):
+def action_mutiplex_8bit(a):
+    """Most starts from g3 least is g1 signifcant bit 2^3"""
+    match a:
+        case 0:
+            # 000
+            robo.write(g1, LOW)
+            robo.write(g2, LOW)
+            robo.write(g3, LOW)
+        case 1:
+            # 001
+            robo.write(g1, HIGH)
+            robo.write(g2, LOW)
+            robo.write(g3, LOW)
+        case 2:
+            # 010
+            robo.write(g1, LOW)
+            robo.write(g2, HIGH)
+            robo.write(g3, LOW)
+        case 3:
+            # 011
+            robo.write(g1, HIGH)
+            robo.write(g2, HIGH)
+            robo.write(g3, LOW)
+        case 4:
+            # 100
+            robo.write(g1, LOW)
+            robo.write(g2, LOW)
+            robo.write(g3, HIGH)
+        case 5:
+            # 101
+            robo.write(g1, HIGH)
+            robo.write(g2, LOW)
+            robo.write(g3, HIGH)
+        case 6:
+            # 110
+            robo.write(g1, LOW)
+            robo.write(g2, HIGH)
+            robo.write(g3, HIGH)
+        case 7:
+            # 111
+            robo.write(g1, HIGH)
+            robo.write(g2, HIGH)
+            robo.write(g3, HIGH)
+        case _:
+            print("not valid I2C location")
+def I2C_Op(v):
+    action_mutiplex_8bit(v)
+
+def action_option(v):
     global state
     """this function does an option according to what is given"""
     #step_size = 10
@@ -57,8 +115,10 @@ def option(v):
             vac_on()
         case "ee":
             vac_off()
+        case "c":
+            I2C_Op(v)
         case _:
-            return Exception
+            print("not valid")
 def vac_on():
     robo.write(v1, HIGH)
 def vac_off():
@@ -96,6 +156,7 @@ def stop():
     robo.write(m4, LOW)
     print("stoped")
 
+
 def intro():
     #set the vac. gpio to output
     robo.set_mode(v1, pigpio.OUTPUT)
@@ -113,6 +174,10 @@ def intro():
     robo.set_mode(g1, pigpio.OUTPUT)
     robo.set_mode(g2, pigpio.OUTPUT)
     robo.set_mode(g3, pigpio.OUTPUT)
+    #set the SCL and SDA pin to high
+    robo.set_pullup_down(SDA, pigpio.PUD_UP)
+    robo.set_pullup_down(SCL, pigpio.PUD_UP)
+
 
 
     #instructions
@@ -124,6 +189,7 @@ def intro():
     print("ee is vac off")
     print("q is stop")
     print("r is quit")
+    print("1-7 I2C chan select")
 
 def read_lm():
     print("reading left motor")
@@ -131,17 +197,34 @@ def read_lm():
 def read_rm():
     print("reading right motor")
 
+def read_I2C(addr=0):
+    print("Starting I2C")
+    hand = robo.i2c_open(BUS, addr)
+    (count, data) = robo.i2c_read_i2c_block_data(hand, REG, 1)
+    if(count >= 0):
+        #what this line does is gets a float and puts it into the string file.
+        print(f"Data read from register 0x{REG:02X}: {data[0]}")
+    else:
+        print("read failed.")
+    robo.i2c_close(hand)
+
 def de_init():
-    print("to do at later day")
+    robo.write(m1, LOW)
+    robo.write(m2, LOW)
+    robo.write(m3, LOW)
+    robo.write(m4, LOW)
+    robo.write(g1, LOW)
+    robo.write(g2, LOW)
+    robo.write(g3, LOW)
 
 def main():
     intro()
     while(state == 1):
         word = input("option?")
-        option(word)
+        action_option(word)
         time.sleep(0.1)
     #exits the program puts
-    """TODO deinit"""
+    de_init()
     robo.stop()
 
 #main
